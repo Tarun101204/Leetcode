53d21d90e015601df3e1973fc94bf44a26518bc5
Tarun101204 <kaushikt697@gmail.com>
2026-09-10 00:56:20 +0530

    Time: 0 ms (100%), Space: 8.8 MB (67.23%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,27 @@
+#include <stack>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (char x : s) {
+            // Push all opening brackets
+            if (x == '(' || x == '[' || x == '{') {
+                st.push(x);
+            }
+            else {
+                if (st.empty()) return false;
+
+                if (x == ')' && st.top() == '(') st.pop();
+                else if (x == ']' && st.top() == '[') st.pop();
+                else if (x == '}' && st.top() == '{') st.pop();
+                else return false;
+            }
+        }
+
+        return st.empty();
+    }
+};
